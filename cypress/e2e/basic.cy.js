8a3bee9f9ec12964c16e717cc6786e9e516d72c7
chengchen <cc8602@gmail.com>
2022-10-23 11:02:02 +0800

    remove h2 check

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -2,10 +2,10 @@ describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
+//  it('displays the resources text', () => {
+//    cy.get('h2')
+//    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
+//  })
   it('renders the image', () => {
     cy.get('img')
     .should('be.visible')
